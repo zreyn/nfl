8d32ccd0e7c1343bd91fbf19815f53cdcd21a522
Zane Reynolds <zane.reynolds@gmail.com>
2016-11-15 16:12:06 -0800

    installed the 2016 model in the app and tried a bunch of different modeling

--- src/app.py
+++ src/app.py
@@ -37,11 +37,11 @@ GLOBALS
 app = Flask(__name__)
 
 # load the current model
-with open('data/model.pkl', 'r') as f:
+with open('data/gbc-v2.pkl', 'r') as f:
     model = pickle.load(f)
 
 # set the model version
-model_version = 'gdbc_v1'
+model_version = 'gbc-v2'
 
 # for now, just use a file to store confusion matrices
 user_cm_file = 'data/user_cm.csv'
